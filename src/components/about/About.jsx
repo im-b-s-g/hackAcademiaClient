e51169428401f286b9f0e41ff5670e8a141c9252
im-b-s-g <brahmgaur123@gmail.com>
2025-03-23 15:46:58 +0530

    updated About Page

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -10,9 +10,9 @@ const About = () => {
         <h1>Hack Academia</h1>
         <p>
           At Hack Academia, we are dedicated to empowering individuals with the
-          knowledge to navigate and secure the digital world. Our mission is to
-          provide accessible, high-quality educational materials that foster a
-          deeper understanding of cybersecurity.
+          knowledge to navigate academics. Our mission is to provide accessible,
+          high-quality educational materials that foster a deeper understanding
+          of respective courses.
         </p>
       </div>
       {/* <div className="About-video"> */}
